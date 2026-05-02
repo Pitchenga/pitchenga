94e15132c515dd3b3f199d50d5be5d7a92deee05
Din Tort <dintort@users.noreply.github.com>
2026-05-03 00:50:32 +0200

    Change branch to production

--- generate_timestamp.cmake
+++ generate_timestamp.cmake
@@ -1,2 +1,2 @@
-string(TIMESTAMP NEW_TIMESTAMP "%y%m%d-%H%M%S")
+string(TIMESTAMP NEW_TIMESTAMP "%y.%m%d.%H%M%S")
 file(WRITE "${OUTPUT_FILE}" "#pragma once\n#define BUILD_TIMESTAMP \"${NEW_TIMESTAMP}\"\n")
