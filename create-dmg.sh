b7962c6a3bc640ecd97d234c36880a593c415916
Din Tort <dintort@users.noreply.github.com>
2026-05-07 17:45:47 +0200

    Arrange DMG icons works

--- create-dmg.sh
+++ create-dmg.sh
@@ -42,7 +42,7 @@ ln -s /Library/Audio/Plug-Ins/VST3 "$STAGING_DIR/VST3"
 
 # Create a temporary read-write DMG
 echo "Building temporary DMG..."
-TEMP_DMG="temp.dmg"
+TEMP_DMG="Pitchenga-tmp.dmg"
 rm -f "$TEMP_DMG"
 hdiutil create -volname "$VOL_NAME" -srcfolder "$STAGING_DIR" -ov -format UDRW -size 300m "$TEMP_DMG"
 
@@ -58,28 +58,28 @@ tell application "Finder"
     set theDisk to disk "$VOL_NAME"
     open theDisk
     set theView to container window of theDisk
-    
+
     set current view of theView to icon view
     set toolbar visible of theView to false
     set statusbar visible of theView to false
     -- {left, top, right, bottom}
     set the bounds of theView to {400, 100, 1000, 550}
-    
+
     set viewOptions to the icon view options of theView
     set icon size of viewOptions to 80
     set text size of viewOptions to 12
     set arrangement of viewOptions to not arranged
-    
+
     -- Positioning pairs
     set position of item "Pitchenga.app" of theDisk to {150, 100}
     set position of item "Applications" of theDisk to {450, 100}
-    
+
     set position of item "Pitchenga.component" of theDisk to {150, 240}
     set position of item "Components" of theDisk to {450, 240}
-    
+
     set position of item "Pitchenga.vst3" of theDisk to {150, 380}
     set position of item "VST3" of theDisk to {450, 380}
-    
+
     update items of theDisk
     delay 5
     close theView
